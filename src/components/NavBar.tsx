1c2efbc3bf481c77237877c757bc6b980e5fac02
sudotx <64108848+sudotx@users.noreply.github.com>
2025-08-11 06:15:12 +0100

    Update NavBar to replace 'NFT' with 'Mint NFT' and remove Farms link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -31,11 +31,8 @@ const NavBar = () => {
           <img src="https://res.cloudinary.com/dma1c8i6n/image/upload/v1753230758/003-piggy-bank_a18vpu.png" alt="" />
           <span className="font-bold">Stake</span>
         </NavLink>
-        <NavLink to="/farms" className="flex items-center gap-1">
-          <img src="https://res.cloudinary.com/dma1c8i6n/image/upload/v1753230759/leaf_bu9iiv.png" alt="" /> <span className="font-bold">Farms</span>
-        </NavLink>
         <NavLink to="/mint" className="flex items-center gap-1">
-          <img src="/farmrpng.png" className="w-6 h-6" alt="NFT" /> <span className="font-bold">NFT</span>
+          <img src="https://res.cloudinary.com/dma1c8i6n/image/upload/v1753230759/leaf_bu9iiv.png" alt="" /> <span className="font-bold">Mint NFT</span>
         </NavLink>
       </nav>
 
@@ -99,11 +96,8 @@ const NavBar = () => {
             <img src="https://res.cloudinary.com/dma1c8i6n/image/upload/v1753230758/003-piggy-bank_a18vpu.png" alt="" />
             <span>Stake</span>
           </NavLink>
-          <NavLink to="/farms" onClick={() => setIsNavOpen(false)} className="flex items-center gap-1 bg-white rounded mb-6 p-2">
-            <img src="https://res.cloudinary.com/dma1c8i6n/image/upload/v1753230759/leaf_bu9iiv.png" alt="" /> <span>Farms</span>
-          </NavLink>
           <NavLink to="/mint" onClick={() => setIsNavOpen(false)} className="flex items-center gap-1 bg-white rounded mb-6 p-2">
-            <img src="/farmrpng.png" className="w-6 h-6" alt="NFT" /> <span>NFT</span>
+            <img src="https://res.cloudinary.com/dma1c8i6n/image/upload/v1753230759/leaf_bu9iiv.png" alt="" /> <span>Mint NFT</span>
           </NavLink>
           <div className="flex justify-center mt-4">
             <ConnectButton />
