4c640c60524a50b6ab998be27913d82dd55e2d2d
sudotx <64108848+sudotx@users.noreply.github.com>
2025-09-29 16:15:41 +0100

    add config for blocx mainnet

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,12 +1,34 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { defineChain } from 'viem/utils';
 import { http } from "wagmi";
-import { sepolia } from "wagmi/chains";
+
+// Define your custom chain
+const customChain = defineChain({
+    id: 879151, // Your chain ID
+    name: 'BLOCX. Mainnet',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'BLOCX',
+        symbol: 'BLOCX',
+    },
+    rpcUrls: {
+        default: {
+            http: ['https://rpc.blocxscan.com'],
+        },
+        public: {
+            http: ['https://rpc.blocxscan.com'],
+        },
+    },
+    blockExplorers: {
+        default: { name: 'Blocx Explorer', url: 'https://blocxscan.com/' },
+    },
+});
 
 export const config = getDefaultConfig({
     appName: "FarmrSwap",
     projectId: "8X1df9Wbcqj6A7LWG71Ra5yLYj-1eL7y",
-    chains: [sepolia],
+    chains: [customChain],
     transports: {
-        [sepolia.id]: http(),
+        [customChain.id]: http(),
     }
 });
