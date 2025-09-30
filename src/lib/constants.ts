1f442c2ecfc02b22909dcaef0a1d01bfe4321776
sudotx <64108848+sudotx@users.noreply.github.com>
2025-09-30 23:02:15 +0100

    updated test tokens

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -19,15 +19,15 @@ export const TOKENS = {
     },
     FARMR: {
         symbol: 'FARMR',
-        name: 'FARMR Token',
-        address: '0x8D3815197b07A24D8Df7f50E28cD47Fe2165d65F',
+        name: 'test FARMR Token',
+        address: '0x6565ddb196392D387e88E0A85e688d1d750CF4A9',
         decimals: 18,
         logoURI: 'https://res.cloudinary.com/dma1c8i6n/image/upload/f_auto,q_auto/farmr_coin_j5gfsd',
         color: ''
     },
     USDC: {
         symbol: 'USDC',
-        name: 'USDC Token',
+        name: 'test USDC Token',
         address: '0x2bc2C528879A025f7e97e68b5142410fF719606B',
         decimals: 6,
         logoURI: 'https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png',
