0675685c58a95fb4027cc0d8bd38786370536b4e
farmrswap <farmrswap@gmail.com>
2025-10-06 19:26:56 +0100

    Updated token names

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -19,25 +19,18 @@ export const TOKENS = {
     },
     FARMR: {
         symbol: 'FARMR',
-        name: 'test FARMR Token',
-        address: '0x6565ddb196392D387e88E0A85e688d1d750CF4A9',
+        name: 'FARMR',
+        address: '0x8D3815197b07A24D8Df7f50E28cD47Fe2165d65F',
         decimals: 18,
         logoURI: 'https://res.cloudinary.com/dma1c8i6n/image/upload/f_auto,q_auto/farmr_coin_j5gfsd',
         color: ''
     },
-    USDC: {
-        symbol: 'USDC',
-        name: 'test USDC Token',
-        address: '0x2bc2C528879A025f7e97e68b5142410fF719606B',
-        decimals: 6,
-        logoURI: 'https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png',
-        color: ''
-    },
+    
 };
 
 export const NATIVE_TOKEN = {
     symbol: 'BLOCX',
-    name: 'BLOCX Token',
+    name: 'BLOCX',
     address: '0x0000000000000000000000000000000000000000', // Native token address
     decimals: 18,
     logoURI: 'https://res.cloudinary.com/dma1c8i6n/image/upload/f_auto,q_auto/blocxcoin_nhozz7',
